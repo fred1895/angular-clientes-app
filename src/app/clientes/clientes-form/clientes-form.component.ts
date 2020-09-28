5d88ff1a74f9cbe53f8a5581d193ff4c9d44ec0c
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-28 14:37:14 -0300

    Starting project

---
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../clientes';
+
+@Component({
+  selector: 'app-clientes-form',
+  templateUrl: './clientes-form.component.html',
+  styleUrls: ['./clientes-form.component.css']
+})
+export class ClientesFormComponent implements OnInit {
+
+  cliente: Cliente;
+
+  constructor() { 
+    this.cliente = new Cliente();
+  }
+
+  onSubmit() {
+    console.log(this.cliente);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
