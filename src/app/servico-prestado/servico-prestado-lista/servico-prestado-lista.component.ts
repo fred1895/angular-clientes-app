5d143f884846764b1136da106053a287e8e3f77e
Fred Almeida <fredericonascimento97@gmail.com>
2020-10-01 11:56:06 -0300

    Implementado servico de busca de clientes via API

--- src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
+import { ServicoPrestadoBusca } from './servico-prestado-busca';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -10,8 +12,11 @@ export class ServicoPrestadoListaComponent implements OnInit {
   nome: string;
   mes: number;
   meses: number[];
+  servicoBusca: ServicoPrestadoBusca[];
 
-  constructor() {
+  constructor(
+    private service: ServicoPrestadoService,
+  ) {
     this.meses = [1,2,3,4,5,6,7,8,9,10,11,12];
    }
 
@@ -20,6 +25,12 @@ export class ServicoPrestadoListaComponent implements OnInit {
 
   consultar(): void {
     console.log(this.nome, this.mes);
+    this.service
+    .buscar(this.nome, this.mes)
+    .subscribe(response => {
+      this.servicoBusca = response;
+    })
+    console.log(this.servicoBusca);
   }
 
 }
